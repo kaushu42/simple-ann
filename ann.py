3c6d99efb51b52afa4b5ec0dca80583eb1403560
Kaushal Raj Mishra <kaushu42@gmail.com>
2018-07-07 10:19:50 +0545

    Completed the code and added documentation

---
+++ ann.py
@@ -0,0 +1,214 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets import load_breast_cancer
+
+class NN:
+    '''
+        An aritificial neural network class created using numpy only
+    '''
+    # Define all the necessary parameters for the network
+    layer_sizes = None # A tuple containing the size of all layers i.e the number of neurons in each layer
+    layer_count = 0 # To store the number of layers in the nework
+    weights = [] # List to store all the weights of the network
+    biases = [] # List to store all the biases of the network
+    a = [] # List that stores the activations. The first element is set to be the input always.
+    z = [] # Activations without applying non-linearity (sigmoid)
+    deltas = [] # The gradients with respect to the activations
+    delta_weights = [] # The gradients to update the weights
+    targets = None # The expected outputs
+    alpha = 0 # The learning rate
+    epoch = 0 # Number of epochs to perform on the data
+
+    def __init__(self, layers, learning_rate = 0.01, epochs = 100):
+        '''
+        Initializes the neural network parameters
+
+        Args:
+            layers: A tuple containing the number of neurons in all layers
+            learning_rate: The learning rate for gradient ascent
+            epochs: The number of iteratons to perform on the data
+
+        Returns:
+            None
+
+        Raises:
+            None
+        '''
+        self.layer_sizes = layers
+        self.layer_count = len(layers)
+        self.alpha = learning_rate
+        self.epoch = epochs
+
+        # We need to initialize the weights randomly but we can initialize the biases to zeros
+        for i in range(self.layer_count - 1):
+            self.weights.append(np.random.randn(layers[i], layers[i + 1]))
+            self.biases.append(np.zeros((1, layers[i + 1])))
+
+    def train(self, verbose = False, plot_costs = True):
+        '''
+        Train the neural network with the loaded data.
+
+        Args:
+            verbose: Displays the progress of training if set to True
+            plot_costs: Plots the costs with each iteration if set to True
+
+        Returns:
+            None
+
+        Raises:
+            None
+
+        '''
+        costs = []
+
+        for i in range(self.epoch):
+            cost = self.forward()
+            costs.append(cost)
+            if(i %100 == 0 and verbose):
+                print('Iteration: %d\tCost: %f'%(i, cost))
+            self.backward()
+
+        if plot_costs:
+            plt.plot(costs)
+            plt.show()
+
+
+    def load_data(self, X, y):
+        '''
+        The activation function for the neural network (sigmoid).
+
+        Args:
+            X: The input matrix to train the network.
+            y: The targets. Encode them as one hot vectors first.
+        Returns:
+            None
+
+        Raises:
+            None
+        '''
+        self.a.append(X)
+        self.targets = y
+
+    @staticmethod
+    def activation(z):
+        '''
+        The activation function for the neural network (sigmoid).
+
+        Args:
+            z: The input array
+
+        Returns:
+            The activation of the input.
+
+        Raises:
+            None
+        '''
+        return 1/(1 + np.exp(-z))
+
+    @staticmethod
+    def _activation(z):
+        '''
+        The derivative of the activation function.
+
+        Args:
+            z: The input array.
+
+        Returns:
+            The derivative of the activation function for the input.
+
+        Raises:
+            None
+        '''
+        out = NN.activation(z)
+        return out*(1 - out)
+
+    def forward(self):
+        '''
+        Perform a forward pass on the neural network to calculate all the activations.
+
+        Args:
+            None
+
+        Returns:
+            The cost of training.
+
+        Raises:
+            None
+        '''
+        self.z = [] # Clear the previous values
+        self.a = [self.a[0]] # Retain only the input from the previously calculated activations
+        for i in range(self.layer_count - 1):
+            self.z.append(self.a[-1].dot(self.weights[i]) + self.biases[i])
+            self.a.append(self.activation(self.z[-1]))
+        return self.cost()
+
+    def backward(self):
+        '''
+        Perform a backward pass on the network to calculate the gradients with respect to the activations, weights and biases.
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            None
+        '''
+        # Calculate the initial delta and clear previous deltas.
+        self.deltas = [(self.targets - self.a[-1])*self._activation(self.z[-1])]
+
+        # Clear the previously calculated gradients for weights and biases
+        self.delta_weights = []
+        self.delta_biases = []
+
+        # Perform backpropagation
+        for i in reversed(range(self.layer_count - 1)):
+            # dC/dz and dC/db calculation
+            self.delta_weights.append(self.a[i].T.dot(self.deltas[-1]))
+            self.delta_biases.append(np.sum(self.deltas[-1], axis = 0, keepdims = True))
+
+            # dC/dz for the preceding layer
+            self.deltas.append(
+                self.deltas[-1].dot(self.weights[i].T) * self._activation(self.z[i-1])
+            )
+
+        # The gradients are calculated backwards. So reverse the list to sort the lists in order.
+        self.delta_weights = self.delta_weights[::-1]
+        self.delta_biases = self.delta_biases[::-1]
+
+        # Perform update on the weights
+        for i in range(self.layer_count-1):
+            self.weights[i] = self.weights[i] + self.alpha * self.delta_weights[i]
+            self.biases[i] = self.biases[i] + self.alpha * self.delta_biases[i]
+
+    def cost(self, epsilon=1e-12):
+        """
+        Computes the cost between targets predictions. Need to encode the targets as one hot vectors.
+
+        Args:
+            epsilon: A small value so that the value of log doesn't go to infinite
+
+        Returns:
+            The cross entropy loss for the predictions.
+        """
+        predictions = np.clip(self.a[-1], epsilon, 1. - epsilon)
+        N = predictions.shape[0]
+        ce = -np.sum(np.sum(self.targets*np.log(predictions + 1e-9) +
+                    (1 - self.targets)*np.log(1 - predictions + 1e-9)))/N
+        return ce
+
+
+if __name__ == '__main__':
+    nn = NN((3, 4, 2, 1), learning_rate = 0.5, epochs = 10000)
+    X = np.array(([0, 0, 0],
+            [0, 0, 1],
+            [0, 1, 0],
+            [0, 1, 1],
+            [1, 0, 0],
+            [1, 0, 1],
+            [1, 1, 0],
+            [1, 1, 1]))
+    y = np.array([[0], [1], [1], [0], [1], [0], [0], [1]])
+    nn.load_data(X, y)
+    nn.train(verbose = True, plot_costs = True)
--- main.py
+++
@@ -1,6 +0,0 @@
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatten, Dropout
-
-model = Sequential()
-model.add(Conv2D())
